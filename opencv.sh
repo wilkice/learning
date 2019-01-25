f6dc2b3ef2eac59a1bc5e488f57ff2b595d3e7b8
darcy <35746578+wilkice@users.noreply.github.com>
2019-01-25 22:20:14 +0800

    Create opencv.sh

---
+++ opencv.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+apt install build-essential cmake git libgtk2.0-dev pkg-config libavcodec-dev libavformat-dev 
+apt install libjpeg-dev libpng-dev libtiff-dev  libswscale-dev python-dev python-numpy libtbb2 libtbb-dev 
+apt install libjasper-dev libdc1394-22-dev numpy

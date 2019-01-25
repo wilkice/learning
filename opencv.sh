0e76429b1414cefa58c7008ed84f1b5e13dd808b
darcy <35746578+wilkice@users.noreply.github.com>
2019-01-25 22:41:34 +0800

    Update opencv.sh

--- opencv.sh
+++ opencv.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
+# opencv安装依赖
 
 apt install build-essential cmake git libgtk2.0-dev pkg-config libavcodec-dev libavformat-dev
 
 apt install libjpeg-dev libpng-dev libtiff-dev  libswscale-dev python-dev python-numpy libtbb2 libtbb-dev
 
-apt install libjasper-dev libdc1394-22-dev numpy
+apt install libdc1394-22-dev 

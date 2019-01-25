e2bed5f694af04d3777419548481548214c4f74a
darcy <35746578+wilkice@users.noreply.github.com>
2019-01-25 22:53:17 +0800

    Update opencv.sh

--- opencv.sh
+++ opencv.sh
@@ -5,4 +5,4 @@ apt install build-essential cmake git libgtk2.0-dev pkg-config libavcodec-dev li
 
 apt install libjpeg-dev libpng-dev libtiff-dev  libswscale-dev python-dev python-numpy libtbb2 libtbb-dev
 
-apt install libdc1394-22-dev 
+apt install libdc1394-22-dev libtiff5-dev:i386 libtiff5-dev
